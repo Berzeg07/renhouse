2d3ee05cf6cc3f13d2d3509a1852a38e74c70fe1
Aslan <berezg86@gmail.com>
2020-05-28 22:39:23 +0300

    Добавил отступы для container

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -1,6 +1,34 @@
 $(document).ready(function() {
-    $('.burger').click(function(){
+    $('.burger').click(function() {
         $(this).toggleClass('is-active');
         $('.mobile-menu, .overlay').fadeToggle();
     });
+
+    var swiper = new Swiper('.filial-slider', {
+        slidesPerView: 7,
+        spaceBetween: 21,
+        centeredSlides: true,
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'progressbar',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            767: {
+                slidesPerView: 2,
+                spaceBetween: 15,
+            },
+            1169: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+        }
+    });
+
+
+
 });
