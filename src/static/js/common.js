3c2f2232c327a3f937aa0c49498db6b238f37691
Aslan <berezg86@gmail.com>
2020-05-31 20:41:48 +0300

    Перенес слайдеры в скрипт

--- src/static/js/common.js
+++ src/static/js/common.js
@@ -14,94 +14,7 @@ $(document).ready(function() {
     //     }
     // };
 
-    $('.burger').click(function() {
-        $(this).toggleClass('is-active');
-        $('.mobile-menu, .overlay').fadeToggle();
-    });
 
-    var swiper = new Swiper('.filial-slider', {
-        slidesPerView: 6,
-        spaceBetween: 21,
-        // centeredSlides: true,
-        slidesPerView: 'auto',
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            767: {
-                slidesPerView: 2,
-                spaceBetween: 15,
-                loop: false,
-            },
-            1169: {
-                slidesPerView: 4,
-                spaceBetween: 40,
-                loop: false,
-            },
-        }
-    });
-
-    var swiper2 = new Swiper('.network-slider', {
-        slidesPerView: 4,
-        spaceBetween: 20,
-        centeredSlides: false,
-        // slidesPerView: 'auto',
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            767: {
-                slidesPerView: 1,
-                spaceBetween: 15,
-                loop: false,
-            },
-            1169: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-                loop: false,
-            },
-        }
-    });
-
-    var swiper3 = new Swiper('.presentation-slider', {
-        slidesPerView: 5,
-        spaceBetween: 34,
-        centeredSlides: false,
-        // slidesPerView: 'auto',
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'progressbar',
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        breakpoints: {
-            767: {
-                slidesPerView: 2,
-                spaceBetween: 15,
-                loop: false,
-            },
-            1169: {
-                slidesPerView: 3,
-                spaceBetween: 40,
-                loop: false,
-            },
-        }
-    });
 
 
     function resizeScrenn() {
@@ -198,5 +111,3 @@ let calculateLinksChecked = document.querySelectorAll('.calculate-link');
 
         this.classList.add('calculate-link_first');
     }
-
-
